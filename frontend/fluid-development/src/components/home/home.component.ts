faf1f996c58f06775e27762bd04818f954d11083
tessnness <ppitubeatrice@gmail.com>
2025-09-17 12:52:04 +0300

    about section

--- frontend/fluid-development/src/components/home/home.component.ts
+++ frontend/fluid-development/src/components/home/home.component.ts
@@ -1,47 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
 import { MenubarModule, Menubar } from 'primeng/menubar';
 import { RouterModule } from "@angular/router";
+import { CountUpModule } from 'ngx-countup';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [RouterModule]
+  imports: [RouterModule, CountUpModule]
 })
 export class HomeComponent implements OnInit {
 
-  items: MenuItem[] | undefined;
   openProjects: boolean | undefined;
+  countOpts = {
+    duration: 2,
+    useGrouping: false,
+  }
+
+  // target = 2000
+
+  // replay(){
+  //   this.target = 0;
+  //   setTimeout(() => {
+  //     this.target = 2000
+  //   }, 0)
+  // }
+
   constructor() { }
 
   ngOnInit() {
-    this.items = [
-      {
-        label: 'Acasă'
-      },
-      {
-        label: 'Despre'
-      },
-      {
-        label: 'Proiecte',
-        items: [
-          {
-            label: 'În desfaşurare',
-            icon: 'pi pi-clock'
-          },
-          {
-            label: 'Încheiate',
-            icon: 'pi pi-check'
-          }
-        ]
-      },
-      {
-        label: 'Contact'
-      }
 
-    ]
   }
+
+
 
 }
